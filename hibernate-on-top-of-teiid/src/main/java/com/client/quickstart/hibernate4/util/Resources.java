190719dfd8de6e5b06c31f5e2510dca7b63e1382
Van Halbert <vhalbert@redhat.com>
2013-06-12 16:40:05 -0500

    Fix hibernate quickstart as well as, changing dynamicvdb-datafederation quickstart to support these changes

--- hibernate-on-top-of-teiid/src/main/java/com/client/quickstart/hibernate4/util/Resources.java
+++ hibernate-on-top-of-teiid/src/main/java/com/client/quickstart/hibernate4/util/Resources.java
@@ -26,10 +26,6 @@ import javax.persistence.PersistenceContext;
 /**
  * This class uses CDI to alias Java EE resources, such as the persistence context, to CDI beans
  * 
- * <p>
- * Example injection on a managed bean field:
- * </p>
- * 
  * <pre>
  * &#064;Inject
  * private EntityManager em;
@@ -47,8 +43,4 @@ public class Resources {
       return Logger.getLogger(injectionPoint.getMember().getDeclaringClass().getName());
    }
    
-//   @PostConstruct
-//   public void postConstruct() {
-//	   em.
-//   }
 }
