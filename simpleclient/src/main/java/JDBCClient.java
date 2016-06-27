aff96981de690ecb6a1530afd8c31a2513c61f04
kylinsoong <kylinsoong.1214@gmail.com>
2016-06-27 12:25:34 +0800

    update/format the document

--- simpleclient/src/main/java/JDBCClient.java
+++ simpleclient/src/main/java/JDBCClient.java
@@ -32,49 +32,64 @@ import org.teiid.jdbc.TeiidStatement;
 @SuppressWarnings("nls")
 public class JDBCClient {
 	
+    public static final String HOST = "host";
+    public static final String PORT = "port";
 	public static final String USERNAME = "username";
 	public static final String PASSWORD = "password";
+	public static final String SHOWPLAN = "showplan";
+	public static final String USE_DATASOURCE = "usedatasource";
 	
+	public static final String HOST_DEFAULT = "localhost";
+	public static final int PORT_DEFAULT = 31000;
 	public static final String USERNAME_DEFAULT = "teiidUser";
 	public static final String PASSWORD_DEFAULT = "password1!";
+	public static final boolean SHOWPLAN_DEFAULT = false;
+	public static final boolean USE_DATASOURCE_DEFAULT = false;
 	
 	public static void main(String[] args) throws Exception {
+	    
 		if (args.length < 4) {
-			System.out.println("usage: JDBCClient <host> <port> <vdb> <sql-command>");
+			System.out.println("usage: JDBCClient<vdb> <sql-command>");
 			System.exit(-1);
 		}
-
-		System.out.println("Executing using the TeiidDriver");
-		boolean isSelect = execute(getDriverConnection(args[0], args[1], args[2]), args[3]);
-
-        // only if its a Select will the SQL be performed again using TeiidDataSource
-		if (isSelect) {
-			System.out.println("-----------------------------------");
-			System.out.println("Executing using the TeiidDataSource");
-			// this is showing how to make a Data Source connection. 
-			execute(getDataSourceConnection(args[0], args[1], args[2]), args[3]);
-		}
+		
+		if(isUseDatasource()){
+		    execute(getDataSourceConnection(getHost(), getPort(), args[2]), args[3]); // Executing using the TeiidDriver"
+		} else {
+		    execute(getDriverConnection(getHost(), getPort(), args[2]), args[3]); // Executing using the TeiidDataSource
+		} 
 	}
 	
-	static Connection getDriverConnection(String host, String port, String vdb) throws Exception {
-		String url = "jdbc:teiid:"+vdb+"@mm://"+host+":"+port+";showplan=on"; //note showplan setting
-		Class.forName("org.teiid.jdbc.TeiidDriver");
-		
+	static Connection getDriverConnection(String host, int port, String vdb) throws Exception {
+	    
+		String url = "jdbc:teiid:" + vdb + "@mm://" + host + ":" + port;
+		if(isShowPlan()){  
+		    url += ";showplan=on"; //note showplan setting
+		}
+		Class.forName("org.teiid.jdbc.TeiidDriver");	
 		return DriverManager.getConnection(url,getUserName(), getPassword());		
 	}
 	
-	static Connection getDataSourceConnection(String host, String port, String vdb) throws Exception {
+	static Connection getDataSourceConnection(String host, int port, String vdb) throws Exception {
 		TeiidDataSource ds = new TeiidDataSource();
 		ds.setDatabaseName(vdb);
 		ds.setUser(getUserName());
 		ds.setPassword(getPassword());
 		ds.setServerName(host);
-		ds.setPortNumber(Integer.valueOf(port));
-		
-		ds.setShowPlan("on"); //turn show plan on
-		
+		ds.setPortNumber(port);
+		if(isShowPlan()){
+		    ds.setShowPlan("on"); //turn show plan on
+		}	
 		return ds.getConnection();
 	}
+	
+	static String getHost() {
+        return (System.getProperties().getProperty(HOST) != null ? System.getProperties().getProperty(HOST) : HOST_DEFAULT );
+    }
+	
+	static int getPort() {
+        return (System.getProperties().getProperty(PORT) != null ? Integer.parseInt(System.getProperties().getProperty(PORT)) : PORT_DEFAULT );
+    }
 	
 	static String getUserName() {
 		return (System.getProperties().getProperty(USERNAME) != null ? System.getProperties().getProperty(USERNAME) : USERNAME_DEFAULT );
@@ -82,6 +97,14 @@ public class JDBCClient {
 	
 	static String getPassword() {
 		return (System.getProperties().getProperty(PASSWORD) != null ? System.getProperties().getProperty(PASSWORD) : PASSWORD_DEFAULT );
+	}
+	
+	static boolean isShowPlan () {
+	    return System.getProperties().getProperty(SHOWPLAN) != null ? Boolean.parseBoolean(System.getProperties().getProperty(SHOWPLAN)) : SHOWPLAN_DEFAULT;
+	}
+	
+	static boolean isUseDatasource() {
+	    return System.getProperties().getProperty(USE_DATASOURCE) != null ? Boolean.parseBoolean(System.getProperties().getProperty(USE_DATASOURCE)) : USE_DATASOURCE_DEFAULT;
 	}
 	
 	public static boolean execute(Connection connection, String sql) throws Exception {
@@ -108,15 +131,18 @@ public class JDBCClient {
 						if (i > 0) {
 							System.out.print(",");
 						}
-						System.out.print(results.getString(i+1));
+						System.out.print(results.getObject(i+1));
 					}
 					System.out.println();
 				}
 				results.close();
 			}
-			System.out.println("Query Plan");
-			System.out.println(statement.unwrap(TeiidStatement.class).getPlanDescription());
 			
+			if(isShowPlan()){
+			    System.out.println("Query Plan");
+	            System.out.println(statement.unwrap(TeiidStatement.class).getPlanDescription());
+			}
+				
 			statement.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
